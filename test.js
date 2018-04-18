d0d2c37190e9dce9531af3e7d409e07d2ebf7118
stuartmurry <stuartmurry93@gmail.com>
2018-04-18 14:30:21 -0600

    Setup Mocha and Mysql

--- test.js
+++ test.js
@@ -1,8 +1,36 @@
-var assert = require('assert');
-describe('Array', function() {
-  describe('#indexOf()', function() {
-    it('should return -1 when the value is not present', function() {
-      assert.equal([1,2,3].indexOf(4), -1);
+var assert = require("assert");
+
+require("dotenv").config();
+var keys = require("./keys.js");
+
+const Sequelize = require("sequelize");
+const sequelize = new Sequelize(keys.mysql.CLEARDB_DATABASE_URL);
+
+describe("Sequelize Mysql Setup", function() {
+  describe("#authenticate()", function() {
+    it("should connect to mysql", function() {
+
+      const User = sequelize.define("user", {
+        firstName: {
+          type: Sequelize.STRING
+        },
+        lastName: {
+          type: Sequelize.STRING
+        },
+        test : {
+          type: Sequelize.STRING
+        }
+      });
+
+      // force: true will drop the table if it already exists
+      User.sync({ force: true }).then(() => {
+        // Table created
+        return User.create({
+          firstName: "John",
+          lastName: "Hancock",
+          test : "sdfadsf"
+        });
+      });
     });
   });
 });
