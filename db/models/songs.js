e2d925e135cc82913daac12ea46680b5b8e3734f
stuartmurry <stuartmurry93@gmail.com>
2018-05-06 12:35:32 -0600

    another try

--- db/models/songs.js
+++ db/models/songs.js
@@ -1,11 +1,9 @@
 'use strict';
 module.exports = (sequelize, DataTypes) => {
   var songs = sequelize.define('songs', {
-    id: DataTypes.INTEGER,
     name: DataTypes.STRING,
     location: DataTypes.STRING,
-    description: DataTypes.STRING,
-    primaryKey: true
+    description: DataTypes.STRING
   }, {});
   songs.associate = function(models) {
     // associations can be defined here
